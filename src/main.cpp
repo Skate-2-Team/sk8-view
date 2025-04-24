e72f5102e0a55c9c312f8ea13e5396960e946ab4
0xdanny <dpaolone03@gmail.com>
2025-04-25 00:48:22 +1000

    Added SDL2 & Vulkan to cmake.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,110 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
+#include <vulkan/vulkan.h>
 #include <iostream>
+#include <vector>
 
-int main()
+int main(int p_argc, char *p_argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::string appName = "Sk8 View";
+
+    // Boilerplate SDL2 + Vulkan setup
+    // In reality we need to set up:
+
+    /*
+
+    Swapchain
+    Render Pass
+    Framebuffers
+    Command Buffers
+    Graphics Pipeline
+    etc
+    etc
+
+    and actually build it out
+    */
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        appName.c_str(),
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
+
+    if (!window)
+    {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Create Vulkan instance
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = appName.c_str();
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    unsigned int extensionCount = 0;
+    if (!SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr))
+    {
+        std::cerr << "SDL_Vulkan_GetInstanceExtensions failed" << std::endl;
+        return -1;
+    }
+
+    std::vector<const char *> extensions(extensionCount);
+    SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensions.data());
+
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
+
+    VkInstance instance;
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+    {
+        std::cerr << "Failed to create Vulkan instance" << std::endl;
+        return -1;
+    }
+
+    // Create Vulkan surface with SDL
+    VkSurfaceKHR surface;
+    if (!SDL_Vulkan_CreateSurface(window, instance, &surface))
+    {
+        std::cerr << "SDL_Vulkan_CreateSurface failed" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Vulkan + SDL2 initialized!" << std::endl;
+
+    // Main loop
+    bool running = true;
+    SDL_Event e;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
+                running = false;
+        }
+
+        // Rendering code would go here
+    }
+
+    // Cleanup
+    vkDestroySurfaceKHR(instance, surface, nullptr);
+    vkDestroyInstance(instance, nullptr);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
